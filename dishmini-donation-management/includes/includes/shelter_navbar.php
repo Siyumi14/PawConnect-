bc3d9187cd0e2a30528dfd2286182b2b4f5bf83c
disminiamiththa-del <disminiamiththa@gmail.com>
2026-08-08 19:45:38 +0530

    Add php files to dishmini
    -donation-management folder

---
+++ dishmini-donation-management/includes/includes/shelter_navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="shelter_dashboard.php">
+            <i class="fas fa-paw"></i> PawConnect - Shelter
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="shelter_profile_settings.php">
+                        <i class="fas fa-user-cog"></i> Profile
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-danger" href="logout.php">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ dishmini-donation-management/includes/shelter_sidebar.php
@@ -0,0 +1,61 @@
+<div class="sidebar col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link" href="shelter_dashboard.php">
+                    <i class="fas fa-tachometer-alt"></i> Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shelter_pets.php">
+                    <i class="fas fa-paw"></i> Animal Management
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shelter_adoptions.php">
+                    <i class="fas fa-file-signature"></i> Adoption Management
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shelter_products.php">
+                    <i class="fas fa-store"></i> Marketplace
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shelter_orders.php">
+                    <i class="fas fa-shopping-cart"></i> Orders
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shelter_donations.php">
+                    <i class="fas fa-donate"></i> Donations
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shelter_donation_tracking.php">
+                    <i class="fas fa-chart-pie"></i> Donation Tracking
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shelter_volunteers.php">
+                    <i class="fas fa-users"></i> Volunteers
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="shelter_profile_settings.php">
+                    <i class="fas fa-cog"></i> Profile Settings
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shelter_about.php">
+                    <i class="fas fa-info-circle"></i> About Us
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-danger" href="../logout.php">
+                    <i class="fas fa-sign-out-alt"></i> Logout
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
